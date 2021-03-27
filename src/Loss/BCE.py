c88efcf2cf10afec65783a787dfd19399d8a0721
daothauvin <49521593+daothauvin@users.noreply.github.com>
2021-03-27 19:20:56 +0100

    [Update] BCE

--- src/Loss/BCE.py
+++ src/Loss/BCE.py
@@ -1,17 +1,18 @@
 import numpy as np
 
-from loss import Loss
+from src.Loss.loss import Loss
 
 
 class BCE(Loss):
-    def forward(self, y, yhat):
+    def forward(self, y, yhat, eps = 10e-10):
         '''
         y et yhat de taille (batch,d)
         batch : nombre d'exemples
         d : nombre de classes
         '''
+       
         # TODO Verifier la dimension de sortie
-        return -(y * np.log(yhat) + (1 - y) * np.log(1 - yhat))
+        return -(y * np.log(yhat+eps) + (1 - y) * np.log(1 - yhat + eps))
 
-    def backward(self, y, yhat):
-        return (y - yhat) / ((yhat - 1) * yhat)
+    def backward(self, y, yhat,  eps = 10e-10):
+        return (y - yhat) / ((yhat - 1) * yhat + eps)
