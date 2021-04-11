eac92893ec5292f51da2039bd9ff865b968dab67
daothauvin <49521593+daothauvin@users.noreply.github.com>
2021-04-11 18:10:10 +0200

    maxPool1D

--- src/Pooling/maxPool1D.py
+++ src/Pooling/maxPool1D.py
@@ -7,16 +7,29 @@ class MaxPool1D(Module):
     def __init__(self, k_size, stride):
         self._k_size = k_size
         self._stride = stride
+        
 
     def forward(self, X):
-        array_pool = np.zeros(((len(X) - self.k_size) / self.stride + 1), len(X[0]))
-        for x in range(0, len(array_pool), self._stride):
-            array_pool[x] = np.max(X[x * self._stride, x * self._stride + self._k_size + 1])
+        array_pool = np.zeros(((X.shape[0],X.shape[1] - self.k_size) // self.stride + 1), X.shape[2])
+        
+        self._argmax_array = np.zeros(((X.shape[0],X.shape[1] - self.k_size) // self.stride + 1), X.shape[2])
+        
+        for x in range(0, array_pool.shape[1]):
+            array_pool[:,x] = np.max(X[:,x * self._stride: x * self._stride + self._k_size], axis = 1)
+            self._argmax_array[:,x] = np.argmax(X[:,x * self._stride: x * self._stride + self._k_size], axis = 1)
         return array_pool
 
     def backward_delta(self, input, delta):
-        new_size = (len(input[0]) - 1) * self._stride + self._k_size
-        array_pool = np.zeros(new_size)
-        for x in range(0, new_size, self._k_size):
-            array_pool[x:x * self._k_size] = delta * input[x]
+        new_size = (input.shape[1] - 1) * self._stride + self._k_size
+        array_pool = np.zeros((input.shape[0],new_size))
+        for x in range(0, new_size):
+            array_pool[:,x] = delta[self._argmax_array[:,x]]
         return array_pool
+    '''
+    def backward_delta(self, input, delta):
+        new_size = (input.shape[1] - 1) * self._stride + self._k_size
+        array_pool = np.zeros((input.shape[0],new_size))
+        for x in range(0, new_size):
+            array_pool[:,x:x * self._k_size] = delta * input
+        return array_pool
+    '''
