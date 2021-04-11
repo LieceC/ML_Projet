65ef82f4682825c67e04ed4cffec65d812cfb44d
LC <cliecealexis@gmail.com>
2021-04-11 16:42:53 +0200

    [ADD] broken maxPool1D

--- src/Pooling/maxPool1D.py
+++ src/Pooling/maxPool1D.py
@@ -4,8 +4,19 @@ from src.Module.module import Module
 
 
 class MaxPool1D(Module):
+    def __init__(self, k_size, stride):
+        self._k_size = k_size
+        self._stride = stride
+
     def forward(self, X):
-        return np.max(0.01*X, X)
+        array_pool = np.zeros(((len(X) - self.k_size) / self.stride + 1), len(X[0]))
+        for x in range(0, len(array_pool), self._stride):
+            array_pool[x] = np.max(X[x * self._stride, x * self._stride + self._k_size + 1])
+        return array_pool
 
     def backward_delta(self, input, delta):
-        return delta * np.where(input < 0, 0.01, 1)
+        new_size = (len(input[0]) - 1) * self._stride + self._k_size
+        array_pool = np.zeros(new_size)
+        for x in range(0, new_size, self._k_size):
+            array_pool[x:x * self._k_size] = delta * input[x]
+        return array_pool
