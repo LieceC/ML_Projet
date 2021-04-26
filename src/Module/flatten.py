1bb283cc6a65daa2adc54366c2f907ef0ad1a3b5
LC <cliecealexis@gmail.com>
2021-04-26 15:57:02 +0200

    Rewriting flatten

--- src/Module/flatten.py
+++ src/Module/flatten.py
@@ -2,15 +2,8 @@ from src.Module.module import Module
 
 
 class Flatten(Module):
-    def __init__(self, length, chan_in):
-        super().__init__()
-        self._chan_in = chan_in
-        self._length = length
-
     def forward(self, X):
-        p = X
-        p = p.reshape(X.shape[0], X.shape[1] * X.shape[2])
-        return p
+        return X.reshape(len(X), -1)
 
     def backward_delta(self, input, delta):
-        return delta.reshape(delta.shape[0], self._length, self._chan_in)
+        return delta.reshape(input.shape)
